60a716024fde8a2e462768a14d074b813997c8e6
Daylend <drkcelestial@gmail.com>
2019-06-23 02:18:12 -0600

    minor changes for optimization

--- redditCNN.py
+++ redditCNN.py
@@ -3,6 +3,7 @@ from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.callbacks import EarlyStopping
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from keras.preprocessing import image
@@ -15,6 +16,8 @@ from io import BytesIO
 from PIL import Image
 import os
 
+imageSize = 64
+
 # Use GPU
 config = tf.ConfigProto(device_count={'GPU': 1, 'CPU':4})
 sess = tf.Session(config=config)
@@ -24,20 +27,23 @@ backend.set_session(sess)
 classifier = Sequential()
 
 # Convolution
-classifier.add(Convolution2D(32, 3, 3, input_shape= (64,64,3), activation= 'relu'))
+classifier.add(Convolution2D(int(imageSize/2), 3, 3, input_shape=(imageSize,imageSize,3), activation='relu'))
 
 # Pooling
-classifier.add(MaxPooling2D(pool_size= (2,2)))
+classifier.add(MaxPooling2D(pool_size=(2,2)))
 
 # Flattening
 classifier.add(Flatten())
 
 # "Full connection" whatever that means
-classifier.add(Dense(output_dim = 128, activation="relu"))
-classifier.add(Dense(output_dim = 1, activation="sigmoid"))
+classifier.add(Dense(output_dim=imageSize*2, activation="relu"))
+classifier.add(Dense(output_dim=1, activation="sigmoid"))
 
 # compile
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# implement early stopping
+es = EarlyStopping(monitor="val_loss", mode="auto", patience=50)
 
 # Rescales, translates, zooms, and flips the image to create more training data
 train_datagen = ImageDataGenerator(
@@ -52,14 +58,14 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 # Define training set
 training_set = train_datagen.flow_from_directory(
     'D:\\HDD Storage\\Pictures\\RedditGear\\Train',
-    target_size=(64,64),
+    target_size=(imageSize,imageSize),
     batch_size=32,
     class_mode='binary')
 
 # Define test set
 test_set = train_datagen.flow_from_directory(
     'D:\\HDD Storage\\Pictures\\RedditGear\\Test',
-    target_size=(64,64),
+    target_size=(imageSize,imageSize),
     batch_size=32,
     class_mode='binary')
 
@@ -68,20 +74,22 @@ loadDataAnswer = input("Load saved weights?")
 if(loadDataAnswer == "n"):
     classifier.fit_generator(
         training_set,
-        steps_per_epoch=300,
-        epochs=1,
+        steps_per_epoch=20,
+        epochs=10,
         validation_data=test_set,
-        validation_steps=800)
+        validation_steps=4,
+        callbacks=[es])
 
     classifier.save_weights("weights.h5")
 else:
     classifier.load_weights("weights.h5")
 
-"""while(True):
+"""
+while(True):
     try:
         imagePath = Path(input("Image path:"))
 
-        test_image = image.load_img(imagePath, target_size=(64,64))
+        test_image = image.load_img(imagePath, target_size=(imageSize,imageSize))
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis = 0)
         result = classifier.predict(test_image)
@@ -93,11 +101,8 @@ else:
 
 # Save predictions to file for viewing
 #answers = open("answers.html", "w")
-
-reddit = praw.Reddit(user_agent="Gear Screenshot Detector by /u/Daylend10",
-                     client_id="haha", client_secret="nonono",
-                     username="ha", password="no")
-subreddit = reddit.subreddit('blackdesertonline')
+reddit = praw.Reddit()
+subreddit = reddit.subreddit('2007scape+shittyrobots+awww+softwaregore+mildlyinteresting+blackdesertonline')
 for submission in subreddit.stream.submissions():
     url = submission.url
     if url.endswith(".png") or url.endswith(".jpg") or url.endswith(".jpeg"):
@@ -126,19 +131,24 @@ for submission in subreddit.stream.submissions():
             newimg.write(img_data)
             newimg.close()
 
-            test_image = image.load_img(tempPath, target_size=(64,64))
+            test_image = image.load_img(tempPath, target_size=(imageSize,imageSize))
             test_image = image.img_to_array(test_image)
             test_image = np.expand_dims(test_image, axis = 0)
             result = classifier.predict(test_image)
 
-            if result[0][0] <= 0.01:
-                os.rename(tempPath, gearPath)
+            if result[0][0] <= 0.5:
+                if not os.path.isfile(gearPath):
+                    os.rename(tempPath, gearPath)
+                else:
+                    os.remove(tempPath)
             else:
-                os.rename(tempPath, notGearPath)
+                if not os.path.isfile(notGearPath):
+                    os.rename(tempPath, notGearPath)
+                else:
+                    os.remove(tempPath)
 
 
             print(str(url)+" ---------------------- "+str(result[0][0]))
 
             #answers.write("<image src=\""+url+"\" /><break /><p>"+str(result)+"</p>")
             print("<image src=\""+str(url)+"\" /><break /><p>"+str(result[0][0])+"</p>", file=open("answers.html", "a"))
-
