817b5b0927c99a1fc1665c3fe3752e3a5a86d71a
Daylend <drkcelestial@gmail.com>
2019-06-27 02:38:49 -0600

    Trying to improve accuracy and failing please send help

--- redditCNN.py
+++ redditCNN.py
@@ -1,9 +1,9 @@
 from keras.models import Sequential
-from keras.layers import Convolution2D
+from keras.layers import Convolution2D, Dropout, BatchNormalization, Activation
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
-from keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from keras.preprocessing import image
@@ -17,6 +17,15 @@ from PIL import Image
 import os
 
 imageSize = 64
+comment = "Hi there! This post has been removed because it appears to contain a gear screenshot." \
+          "\n\nWe receive a high volume of people frequently asking for help on gear progression or grinding spots. " \
+          "There are helpful links located at the top of the subreddit, but here are some answers to frequently asked questions:" \
+          "\n\n\n[What gear should I go for next?](https://grumpygreen.cricket/bdo-gear-progression-guide/)" \
+          "\n\n[Where should I grind?](https://docs.google.com/spreadsheets/d/1gPOFA0uMh_Xc6_pZ_e7wjXRsmkQ3wDuNmhSqiahlvTw/edit#gid=761402636)" \
+          "\n\n\nIf you have any further questions or need more help, feel free to leave a comment in the Daily FAQ thread located at the top of the subreddit.  " \
+          "\n\n^(Note: I am a bot, and this feature is currently in beta! Sometimes I make mistakes! My last recorded" \
+          " accuracy is 92% against 350 test examples. If you wish to leave feedback about the bot, feel free to PM)" \
+          " /u/Daylend10 ^(or leave us a message in modmail.)"
 
 # Use GPU
 config = tf.ConfigProto(device_count={'GPU': 1, 'CPU':4})
@@ -27,23 +36,31 @@ backend.set_session(sess)
 classifier = Sequential()
 
 # Convolution
-classifier.add(Convolution2D(int(imageSize/2), 3, 3, input_shape=(imageSize,imageSize,3), activation='relu'))
+#classifier.add(Convolution2D(int(imageSize/2), 3, 3, input_shape=(imageSize,imageSize,3), activation='relu'))
+classifier.add(Convolution2D(int(imageSize/4), 3, 1, input_shape=(imageSize, imageSize, 3)))
+classifier.add(BatchNormalization(epsilon=0.001))
+classifier.add(Activation("relu"))
 
 # Pooling
-classifier.add(MaxPooling2D(pool_size=(2,2)))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Dropout regularization
+#classifier.add(Dropout(0.2))
 
 # Flattening
 classifier.add(Flatten())
 
 # "Full connection" whatever that means
-classifier.add(Dense(output_dim=imageSize*2, activation="relu"))
-classifier.add(Dense(output_dim=1, activation="sigmoid"))
+classifier.add(Dense(units=imageSize*2, activation="relu"))
+classifier.add(Dense(units=1, activation="sigmoid"))
 
 # compile
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # implement early stopping
-es = EarlyStopping(monitor="val_loss", mode="auto", patience=50)
+es = EarlyStopping(monitor="val_acc", mode="auto", patience=100, verbose=1)
+
+mc = ModelCheckpoint("checkpoint.h5", monitor="val_acc", save_best_only=True, mode="max", verbose=1)
 
 # Rescales, translates, zooms, and flips the image to create more training data
 train_datagen = ImageDataGenerator(
@@ -71,19 +88,22 @@ test_set = train_datagen.flow_from_directory(
 
 # Optionally load weights, otherwise start training
 loadDataAnswer = input("Load saved weights?")
-if(loadDataAnswer == "n"):
+if loadDataAnswer == "n":
     classifier.fit_generator(
         training_set,
-        steps_per_epoch=20,
-        epochs=10,
+        steps_per_epoch=200,
+        epochs=5,
         validation_data=test_set,
-        validation_steps=4,
-        callbacks=[es])
+        validation_steps=20,
+        callbacks=[es, mc])
 
     classifier.save_weights("weights.h5")
 else:
     classifier.load_weights("weights.h5")
 
+test_set.reset()
+#eval = classifier.evaluate_generator(generator=test_set, steps=10, verbose=1)
+#print(eval)
 """
 while(True):
     try:
@@ -102,9 +122,11 @@ while(True):
 # Save predictions to file for viewing
 #answers = open("answers.html", "w")
 reddit = praw.Reddit()
-subreddit = reddit.subreddit('2007scape+shittyrobots+awww+softwaregore+mildlyinteresting+blackdesertonline')
+#subreddit = reddit.subreddit('2007scape+shittyrobots+awww+softwaregore+mildlyinteresting+blackdesertonline')
+subreddit = reddit.subreddit('blackdesertonline')
 for submission in subreddit.stream.submissions():
     url = submission.url
+    permalink = submission.permalink
     if url.endswith(".png") or url.endswith(".jpg") or url.endswith(".jpeg"):
         filename = url.rsplit('/', 1)[1]
         h = requests.head(url, allow_redirects=True)
@@ -131,24 +153,31 @@ for submission in subreddit.stream.submissions():
             newimg.write(img_data)
             newimg.close()
 
-            test_image = image.load_img(tempPath, target_size=(imageSize,imageSize))
+            test_image = image.load_img(tempPath, target_size=(imageSize, imageSize))
             test_image = image.img_to_array(test_image)
-            test_image = np.expand_dims(test_image, axis = 0)
+            test_image = np.expand_dims(test_image, axis=0)
             result = classifier.predict(test_image)
 
-            if result[0][0] <= 0.5:
+            if result[0][0] <= 0.1:
+                # If the file exists, we've already seen the post, so do nothing
                 if not os.path.isfile(gearPath):
                     os.rename(tempPath, gearPath)
+                    print(
+                        "https://reddit.com" + str(permalink) + " \t\t\t " + str(url) + " \t\t\t " + str(result[0][0]))
+                    print(subreddit.name)
                 else:
                     os.remove(tempPath)
             else:
                 if not os.path.isfile(notGearPath):
                     os.rename(tempPath, notGearPath)
+                    print(
+                        "https://reddit.com" + str(permalink) + " \t\t\t " + str(url) + " \t\t\t " + str(result[0][0]))
+                    print(subreddit.name)
                 else:
                     os.remove(tempPath)
 
 
-            print(str(url)+" ---------------------- "+str(result[0][0]))
+
 
             #answers.write("<image src=\""+url+"\" /><break /><p>"+str(result)+"</p>")
-            print("<image src=\""+str(url)+"\" /><break /><p>"+str(result[0][0])+"</p>", file=open("answers.html", "a"))
+            #print("<image src=\""+str(url)+"\" /><break /><p>"+str(result[0][0])+"</p>", file=open("answers.html", "a"))
